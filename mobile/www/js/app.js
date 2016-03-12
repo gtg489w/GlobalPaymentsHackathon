72b569f4bfb9374046b90ca386817655ba53f7aa
gtg489w <gtg489w@gmail.com>
2016-03-12 05:27:33 -0500

    working on gps stuff

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -3,22 +3,69 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaGeolocation, $http) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    var watchOptions = {
+   timeout : 3000,
+   enableHighAccuracy: false
+  };
+
+  var watch = $cordovaGeolocation.watchPosition(watchOptions);
+  watch.then(
+   null,
+   function(err) {
+     // error
+     //window.alert(JSON.stringify(err));
+   },
+   function(position) {
+     var lat  = position.coords.latitude;
+     var lng = position.coords.longitude;
+     // window.alert(lat + ', ' + lng);
+
+
+
+     $http({
+      method: 'HEAD',
+      url: 'https://s3.amazonaws.com/2015gphackathon/uploads/location',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(function(result) {
+      window.alert(JSON.stringify(result));
+    }, function(err) {
+      window.alert('err');
+    });
+
+   //   $http({
+   //      method: 'POST',
+   //      url: 'https://fg8ekf1vhl.execute-api.us-east-1.amazonaws.com/prod/facialRecognition',
+   //      headers: {
+   //        'Accept': 'application/json',
+   //        'Content-Type': 'application/json',
+   //        'x-api-key': 'KCyM96hBCC9vQGpQESk7o1I8HmWoYc6f7Qrhn9SL'
+   //      },
+   //      data: {
+   //        "toggleLocation": "receipt"
+   //      }
+   //    }).then(function(result) {
+   //      $scope.state.location = true;
+   //    }, function(err) {});
+
+   // });
+
+
+
+
   });
 })
