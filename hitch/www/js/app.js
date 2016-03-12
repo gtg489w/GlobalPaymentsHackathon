19625a9cc41dcf52d9fa85ea8c60972ec4d9f95a
gtg489w <gtg489w@gmail.com>
2016-03-11 21:06:57 -0500

    Touch ID is working and added camera plugin

--- hitch/www/js/app.js
+++ hitch/www/js/app.js
@@ -1,5 +1,5 @@
 angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaStatusbar) {
 	$ionicPlatform.ready(function() {
 		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
 			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -8,14 +8,20 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
 		if (window.StatusBar) {
 			StatusBar.styleDefault();
 		}
+
+		$cordovaStatusbar.hide();
 	});
 })
 .config(function($stateProvider, $urlRouterProvider) {
-	$stateProvider.state('main', {
+	$stateProvider.state('splash', {
+		url: '/splash',
+		templateUrl: 'templates/splash.html',
+		controller: 'SplashCtrl'
+	})
+	.state('main', {
 		url: '/main',
 		templateUrl: 'templates/main.html',
 		controller: 'MainCtrl'
 	})
-	$urlRouterProvider.otherwise('/main');
-
+	$urlRouterProvider.otherwise('/splash');
 });
--- hitch/www/js/controllers.js
+++ hitch/www/js/controllers.js
@@ -1,22 +1,30 @@
 angular.module('starter.controllers', [])
+
+.controller('SplashCtrl', function($scope) {
+	
+})
 .controller('MainCtrl', function($scope, $cordovaTouchID) {
+
+
+	
+
+
+
+
+
+
 
 
 
 
 	$cordovaTouchID.checkSupport().then(function() {
 		window.alert('supported!');
-		// success, TouchID supported
-	}, function (error) {
-		window.alert(error); // TouchID not supported
-	});
+	}, function (error) {});
 
 	$cordovaTouchID.authenticate("text").then(function() {
 		window.alert('booya');
 		// success
-	}, function () {
-		// error
-		window.alert('error');
-	});
+
+	}, function () {});
 	
 });
